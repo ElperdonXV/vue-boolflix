1d343aee66f5971204cbfe5d8c5c409f48136e4e
ElperdonXV <davidemartinografico@gmail.com>
2022-01-19 11:29:06 +0100

    configuration

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>ciao</h1>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data(){
+        
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>vue</h1>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data(){
+
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
