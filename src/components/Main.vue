95088932affa3827102461efd63b0410e04f1c50
ElperdonXV <davidemartinografico@gmail.com>
2022-01-19 12:05:40 +0100

    progress

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,46 @@
 <template>
-  <h1>vue</h1>
+  <div class="container">
+      <Search
+        @emitSearch="getCard($event)" 
+      />
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Search from './Search.vue';
 export default {
     name: 'Main',
     data(){
-
-    }
+        return {
+        queryPath: 'https://api.themoviedb.org/3/search/movie',
+        queryKey: '6a1be3e2f2a8c68b3ee64e9d5b7d296b',
+        textSearch: '',
+        card: null,
+    };
+},
+    components: {
+        Search,
+    },
+    methods: {
+        getCard(text){
+            this.textSearch = text;
+            this.card = null;
+            axios.get(
+                this.queryPath, {
+                    params: {
+                        api_key: this.queryKey,
+                        query: this.textSearch,
+                    }
+                }
+            ).then((result)=>{
+                this.card = result.data.results;
+                console.log(result.data.results);
+            }).catch((error)=>{
+                console.log(error);
+            });
+        },
+}, 
 }
 </script>
 
