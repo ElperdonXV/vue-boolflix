ff9cafedd55159fa8a0ac1d577f952e1184b3492
ElperdonXV <davidemartinografico@gmail.com>
2022-01-20 15:56:21 +0100

    trying to fix

--- src/App.vue
+++ src/App.vue
@@ -19,29 +19,64 @@ export default {
 },
 data(){
         return {
-        queryPath: 'https://api.themoviedb.org/3/search/movie',
+        queryPath: 'https://api.themoviedb.org/3/search/',
         queryKey: '6a1be3e2f2a8c68b3ee64e9d5b7d296b',
         textSearch: '',
         cards: null,
+        arrayFilm: null,
+        arraySeries: null,
     };
 },
+watch:{
+  printCard(){
+    this.callMovies();
+    this.callSeries();
+    this.mergeArray();
+  }
+},
+
  methods: {
-        getCard(text){
-            this.textSearch = text;
-            this.card = null;
-            axios.get(
-                this.queryPath, {
-                    params: {
-                        api_key: this.queryKey,
-                        query: this.textSearch,
-                    }
-                }
+        
+        callMovies(){
+          this.arrayFilm = null;
+          const movie = 'movie';
+          const parametri= {
+            api_key: this.queryKey,
+            query: this.textSearch
+          }
+            axios.get(`${this.queryPath}${movie}`, { params: parametri }
             ).then((result)=>{
-                this.cards = result.data.results;
+                this.arrayFilm = result.data.results;
                 console.log(result.data.results);
             }).catch((error)=>{
                 console.log(error);
             });
+        },
+
+        callSeries(){
+          this.arraySeries = null;
+          const tv = 'tv';
+          const parametri= {
+            api_key: this.queryKey,
+            query: this.textSearch
+          }
+            axios.get(`${this.queryPath}${tv}`, { params: parametri }
+            ).then((result)=>{
+                this.arraySeries = result.data.results;
+                console.log(result.data.results);
+            }).catch((error)=>{
+                console.log(error);
+            });
+        },
+
+        getCard(text){
+            if (text != '') {
+            this.textSearch = text;
+        }
+        },
+
+        mergeArray(){
+          this.cards = [...this.arrayFilm, ...this.arraySeries];
         },
 }, 
 }
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="container">
-      <div v-for="(card, index) in cards" :key="index" class="cards">
-          <div class="card">
+      <div class="row">
+          <div v-for="(card, index) in cards" :key="index" class="cards col">
+            <div class="card">
               <h2>{{card.title}}</h2>
               <h3>{{card.original_title}}</h3>
               <h4>{{card.original_language}}</h4>
               <h4>{{card.vote_average}}</h4>
-          </div>
+            </div>
+        </div>
       </div>
   </div>
 </template>
