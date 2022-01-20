c52a31f9937eddce9143b7f71affc219a1cfbf3e
ElperdonXV <davidemartinografico@gmail.com>
2022-01-20 17:01:41 +0100

    milestone 2 done

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,6 +2,7 @@
             <div class="card col-4">
               <h2>{{(arname.title) ? arname.title : arname.name}}</h2>
               <h3>{{(arname.original_title) ? arname.original_title : arname.original_name}}</h3>
+              <i :class="'flag flag-' + country(arname.original_language)" />
               <h4>{{arname.original_language}}</h4>
               <h4>{{arname.vote_average}}</h4>
             </div>
@@ -14,12 +15,18 @@ props: {
     arname: {
         type: Object,
     }
-}, 
-methods: {
-
 },
+methods: {
+    country(nation){
+        if (nation === 'en'){
+            return 'us';
+        }
+        return nation;
+    }
+} 
 }
 </script>
 
 <style lang="scss">
+@import '~mdb-ui-kit/css/mdb.min.css';
 </style>
